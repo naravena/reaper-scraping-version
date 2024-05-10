582434a4a6165ff6ee94b0fc804e2c785d93c72b
Nicolas <nico2361993@gmail.com>
2024-05-11 01:33:13 +0200

    comparacion

--- scripts/core.py
+++ scripts/core.py
@@ -1,14 +1,18 @@
+import os
 import re
 
 from utils import get_page_content, extract_text, URL, CSS_SELECTOR
 
+
+def comparison():
+    try:
+        if extract_text(get_page_content(URL), CSS_SELECTOR) != os.environ['PREVIOUS_STATUS']:
+            return True
+        else:
+            return False
+    except Exception as e:
+        raise f"Se produjo un error: {str(e)}"
+
+
 if __name__ == "__main__":
-    def current_state():
-        try:
-            aux = extract_text(get_page_content(URL), CSS_SELECTOR)
-            return re.sub(r'[^\w\s.-]', '', aux)
-        except Exception as e:
-            raise f"Se produjo un error: {str(e)}"
-
-
-    print(current_state())
+    comparison()
