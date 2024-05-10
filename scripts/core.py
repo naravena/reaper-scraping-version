6183b7029f4a3dc7fe5212b3d51e0a5ccbf8b9cd
Nicolas <nico2361993@gmail.com>
2024-05-11 01:37:31 +0200

    comparacion

--- scripts/core.py
+++ scripts/core.py
@@ -5,13 +5,10 @@ from utils import get_page_content, extract_text, URL, CSS_SELECTOR
 
 
 def comparison():
-    try:
-        if extract_text(get_page_content(URL), CSS_SELECTOR) != os.environ['PREVIOUS_STATUS']:
-            return True
-        else:
-            return False
-    except Exception as e:
-        raise f"Se produjo un error: {str(e)}"
+    if extract_text(get_page_content(URL), CSS_SELECTOR) != os.environ['PREVIOUS_STATUS']:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
