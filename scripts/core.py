3ca52f2c919c172ecbb2f3c47c3e2fb46ab5b1aa
Nicolas <nico2361993@gmail.com>
2024-05-11 00:30:31 +0200

    refactor

--- scripts/core.py
+++ scripts/core.py
@@ -2,16 +2,10 @@ import os
 from utils import get_page_content, extract_text, URL, CSS_SELECTOR
 
 if __name__ == "__main__":
-    def check_status():
+    def current_state():
         try:
-            current_state = extract_text(get_page_content(URL), CSS_SELECTOR)
-            previous_state = os.environ['PREVIOUS_STATUS']
-            if current_state != previous_state:
-                return True
-            else:
-                return False
+            aux = extract_text(get_page_content(URL), CSS_SELECTOR)
+            print('CURRENT_STATE  ----> ', aux)
+            return aux
         except Exception as e:
             raise f"Se produjo un error: {str(e)}"
-
-
-    print(check_status())
