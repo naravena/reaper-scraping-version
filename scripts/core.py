823b414c46a1b7b532a6b2bd3f22d4d4258dc329
Nicolas <nico2361993@gmail.com>
2024-05-11 00:45:49 +0200

    refactor

--- scripts/core.py
+++ scripts/core.py
@@ -4,8 +4,7 @@ from utils import get_page_content, extract_text, URL, CSS_SELECTOR
 if __name__ == "__main__":
     def current_state():
         try:
-            aux = extract_text(get_page_content(URL), CSS_SELECTOR)
-            return aux
+            return extract_text(get_page_content(URL), CSS_SELECTOR)
         except Exception as e:
             raise f"Se produjo un error: {str(e)}"
 
