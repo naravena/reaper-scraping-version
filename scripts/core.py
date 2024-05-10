7a5de4e0d64c025c3e7777543518a55d9f8031c3
Nicolas <nico2361993@gmail.com>
2024-05-11 00:37:43 +0200

    refactor

--- scripts/core.py
+++ scripts/core.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     def current_state():
         try:
             aux = extract_text(get_page_content(URL), CSS_SELECTOR)
-            print('CURRENT_STATE  ----> ', aux)
             return aux
         except Exception as e:
             raise f"Se produjo un error: {str(e)}"
+
+
+    print(current_state())
