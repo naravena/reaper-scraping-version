13dc9e4a9ca04d0d7ce23500f483870d4a1d9d3a
Nicolas <nico2361993@gmail.com>
2024-05-11 01:06:32 +0200

    limpiar cadena

--- scripts/core.py
+++ scripts/core.py
@@ -1,10 +1,12 @@
-import os
+import re
+
 from utils import get_page_content, extract_text, URL, CSS_SELECTOR
 
 if __name__ == "__main__":
     def current_state():
         try:
-            return extract_text(get_page_content(URL), CSS_SELECTOR)
+            aux = extract_text(get_page_content(URL), CSS_SELECTOR)
+            return re.sub(r'[^a-zA-Z0-9\s]', '', aux)
         except Exception as e:
             raise f"Se produjo un error: {str(e)}"
 
