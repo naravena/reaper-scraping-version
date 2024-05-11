93de8403a4958a98f223cb1b18d055e0c41bd4d5
Nicolas <nico2361993@gmail.com>
2024-05-12 00:43:30 +0200

    logic change

--- scripts/core.py
+++ scripts/core.py
@@ -41,5 +41,5 @@ def main():
 
 if __name__ == "__main__":
     is_change_version, status_message_text = main()
-    os.environ['IS_CHANGE_VERSION'] = str(is_change_version)
-    os.environ['STATUS_MESSAGE_TEXT'] = status_message_text
+    # os.environ['IS_CHANGE_VERSION'] = str(is_change_version)
+    # os.environ['STATUS_MESSAGE_TEXT'] = status_message_text
