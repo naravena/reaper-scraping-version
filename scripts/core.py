dcb432422d3ac932f07ea874220062d1194a9100
Nicolas <nico2361993@gmail.com>
2024-05-12 02:30:24 +0200

    set variables

--- scripts/core.py
+++ scripts/core.py
@@ -42,7 +42,5 @@ def main():
 
 
 if __name__ == "__main__":
-    env_file = os.getenv('GITHUB_ENV')  # Get the path of the runner file
-    # write to the file
-    with open(env_file, "a") as env_file:
-        env_file.write(f"IS_CHANGE_VERSION={str(main()[0])};STATUS_MESSAGE_TEXT={main()[1]}")
+    os.environ['IS_CHANGE_VERSION'] = str(main()[0])
+    os.environ['STATUS_MESSAGE_TEXT'] = main()[1]
