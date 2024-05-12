83ab7afed610ac326fe2ac5ead3d218fd295a714
Nicolas <nico2361993@gmail.com>
2024-05-12 02:09:26 +0200

    set variables

--- scripts/core.py
+++ scripts/core.py
@@ -42,4 +42,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    env_file = os.getenv('GITHUB_ENV')  # Get the path of the runner file
+    # write to the file
+    with open(env_file, "a") as env_file:
+        env_file.write(f"a={str(main()[0])}")
+        env_file.write(f"b={str(main()[1])}")
+
