cda55ea482263134f8e674af7f69353c2a1c9646
Nicolas <nico2361993@gmail.com>
2024-05-15 14:30:16 +0200

    version python, remove comments

--- scripts/send_whatsapp_message.py
+++ scripts/send_whatsapp_message.py
@@ -3,8 +3,5 @@ from utils import get_environment_variable, params_to_send_whatsapp_message
 
 # Ejemplo de uso
 if __name__ == "__main__":
-    # Crear una instancia de WhatsAppMessageSender
     whatsapp_sender = WhatsAppMessageSender(params_to_send_whatsapp_message)
-
-    # Enviar el mensaje
     whatsapp_sender.send_message(get_environment_variable('MESSAGE'))
