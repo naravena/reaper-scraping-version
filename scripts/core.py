94752246394172a4f9f3931b3bd0c233a7d20fab
Nicolas <nico2361993@gmail.com>
2024-05-15 13:48:46 +0200

    Añado funcionamiento para probar mas paginas en el mismo proyecto

--- scripts/core.py
+++ scripts/core.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 from whatsapp_app_message_sender import WhatsAppMessageSender
-from utils import get_environment_variable, CSS_SELECTOR, URL
+from utils import get_environment_variable, webs_to_scraping, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, \
+    TWILIO_PHONE_NUMBER, WHATSAPP_PHONE_NUMBER
 
 
-def get_current_status():
-    response = requests.get(URL)
+def get_current_status(selector):
+    response = requests.get(selector)
     soup = BeautifulSoup(response.content, 'html.parser')
-    status_element = soup.select_one(CSS_SELECTOR)
+    status_element = soup.select_one(selector)
     if status_element:
         return status_element.get_text(strip=True)
     return None
@@ -22,20 +23,24 @@ def status_message(previous_status, current_status):
             '''
 
 
+def send_message_by_whatsapp(pre_status, post_status):
+    whatsapp_sender = WhatsAppMessageSender(get_environment_variable(TWILIO_ACCOUNT_SID),
+                                            get_environment_variable(TWILIO_AUTH_TOKEN),
+                                            get_environment_variable(TWILIO_PHONE_NUMBER),
+                                            get_environment_variable(WHATSAPP_PHONE_NUMBER))
+    print(status_message(pre_status, post_status))
+    whatsapp_sender.send_message(status_message(pre_status, post_status))
+
+
 def main():
     try:
-        current_status = get_current_status()
-        is_change_in_version = get_environment_variable('PREVIOUS_STATUS') != current_status
-        if current_status is not None and is_change_in_version:
-            print(status_message(get_environment_variable('PREVIOUS_STATUS'), current_status))
-            # Crear una instancia de WhatsAppMessageSender
-            whatsapp_sender = WhatsAppMessageSender(get_environment_variable('TWILIO_ACCOUNT_SID'),
-                                                    get_environment_variable('TWILIO_AUTH_TOKEN'),
-                                                    get_environment_variable('TWILIO_PHONE_NUMBER'),
-                                                    get_environment_variable('WHATSAPP_PHONE_NUMBER'))
-            whatsapp_sender.send_message(status_message(get_environment_variable('PREVIOUS_STATUS'), current_status))
-        else:
-            print("NO HAY NUEVA VERSION")
+        for page, (url, status) in webs_to_scraping.items():
+            current_status = get_current_status(url)
+            is_change_in_version = status != current_status
+            if current_status is not None and is_change_in_version:
+                send_message_by_whatsapp(status, current_status)
+            else:
+                print(f"NO HAY NUEVA VERSION PARA {page}")
     except Exception as e:
         print("Se produjo un error durante la ejecución de main():", e)
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -10,14 +10,19 @@ def get_environment_variable(var_name, default=""):
 
 
 # Variables globales
-URL = "https://www.reaper.fm/download.php"
-CSS_SELECTOR = "div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.downloadinfo"
-NO_STATE_MESSAGE = "No hay estado anterior registrado."
-NO_ELEMENT_MESSAGE = "No se encontró ningún elemento con el selector CSS especificado."
-CHANGE_MESSAGE = "¡La versión se ha actualizado!"
-PREVIOUS_STATUS = get_environment_variable('PREVIOUS_STATUS')
-TWILIO_ACCOUNT_SID = get_environment_variable('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = get_environment_variable('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = get_environment_variable('TWILIO_PHONE_NUMBER')
-WHATSAPP_PHONE_NUMBER = get_environment_variable('WHATSAPP_PHONE_NUMBER')
-MESSAGE = get_environment_variable('MESSAGE')
+REAPER_URL = "https://www.reaper.fm/download.php"
+OBS_URL = "https://obsproject.com/es/download"
+REAPER_CSS_SELECTOR = "div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.downloadinfo"
+OBS_CSS_SELECTOR = "#win_blurb > div.dl_box > span.dl_ver"
+REAPER_PREVIOUS_STATUS = 'REAPER_PREVIOUS_STATUS'
+OBS_PREVIOUS_STATUS = 'OBS_PREVIOUS_STATUS'
+TWILIO_ACCOUNT_SID = 'TWILIO_ACCOUNT_SID'
+TWILIO_AUTH_TOKEN = 'TWILIO_AUTH_TOKEN'
+TWILIO_PHONE_NUMBER = 'TWILIO_PHONE_NUMBER'
+WHATSAPP_PHONE_NUMBER = 'WHATSAPP_PHONE_NUMBER'
+MESSAGE = 'MESSAGE'
+
+webs_to_scraping = {
+    'REAPER': [get_environment_variable(REAPER_URL), get_environment_variable(REAPER_PREVIOUS_STATUS)],
+    'OBS': [get_environment_variable(OBS_URL), get_environment_variable(OBS_PREVIOUS_STATUS)]
+}
